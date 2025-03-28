ad716bf2995f82e97db9b509bef9132bd5e40fe0
deependra-parmar <dparmar@qodeleaf.com>
2025-03-28 12:43:37 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
+<<<<<<< HEAD
 # Terraform
+=======
+# Terraform
+IaC platform or tool helps in provisioning, managing and automating the cloud infrastructure as code.
+
+---
+
+In this one, we are going to deploy a static html, css, js website on Bucket in GCP, attach it with ELB, followed by a Cloud CDN and then adding a DNS entry using Cloud DNS.
+
+Let's get started
+
+```
+main.tf : consists of main resources like s3 bucket, elb, etc
+
+provider.tf: configures the gcp provider for the project
+
+variables.tf: Used to declare the variables.
+
+terraform.tfvars: Assign values to variables. Generally used for assigning values in different envs.
+```
+
+---
+
+After understanding all this, I have executed the following command in the /infra directory
+
+```
+terraform init
+```
+
+which brings the .terraform for storing the states of the application and its deployment. The command also brings and configures the GCP provider for the project.
+
+---
+
+### States in Terraform
+Used to memoize or remember what are the resources that were deployed last time you run terraform apply in order to keep track of changes and what not to deploy again.
+In a nutshell, the statefile is the copy of your current deployment in the cloud.
+
+Now, we can execute the following command to see what are the different operations terraform is going to create or do in the infrastructure with the current configuration and shows the list of all of them.
+
+```
+terraform plan
+```
+
+and after reviewing the plan logs if everything seems fine to you, you can apply to provision the resources and go ahead.
+
+```
+terraform apply
+```
+>>>>>>> 840c90f (Initial Commit)
---
+++ infra/main.tf
@@ -0,0 +1,90 @@
+locals {
+  website_files = [
+    "index.html",
+    "index.css",
+    "index.js",
+    "beep.wav",
+    "success.mp3"
+  ]
+}
+
+# Bucket to store website
+resource "google_storage_bucket" "website" {
+  name = "example-website-bucket-by-dp"
+  location = "US"
+}
+
+# Making the bucket publicly accessible
+resource "google_storage_object_access_control" "public_rule" {
+  bucket = google_storage_bucket.website.name
+  for_each = google_storage_bucket_object.website_files
+  object = each.value.name
+  role = "READER"
+  entity = "allUsers"
+}
+
+# Uploading the static files in the bucket
+resource "google_storage_bucket_object" "website_files" {
+  for_each = toset(local.website_files)
+  name = each.value
+  source = "../website/${each.value}"
+  bucket = google_storage_bucket.website.name
+}
+
+
+# Reserve an external IP Address
+resource "google_compute_global_address" "website_ip" {
+  name = "website-lb-ip"
+}
+
+# Getting the managed DNS Zone
+data "google_dns_managed_zone" "dns_zone" {
+  name = "terraform-gcp"
+}
+
+# Adding the IP to the DNS
+resource "google_dns_record_set" "game" {
+  name = "${data.google_dns_managed_zone.dns_zone.dns_name}"
+  type = "A"
+  ttl = 300
+  managed_zone = data.google_dns_managed_zone.dns_zone.name
+  rrdatas = [google_compute_global_address.website_ip.address]
+}
+
+# Add a bucket as a CDN's source
+resource "google_compute_backend_bucket" "website-backend" {
+  name = "website-bucket"
+  bucket_name = google_storage_bucket.website.name
+  description = "Contains file need for the website"
+  enable_cdn = true
+}
+
+# GCP URL Map
+resource "google_compute_url_map" "website-map" {
+  name = "website-url-map"
+  default_service = google_compute_backend_bucket.website-backend.self_link
+  host_rule {
+    hosts = ["*"]
+    path_matcher = "allpaths"
+  }
+  path_matcher {
+    name = "allpaths"
+    default_service = google_compute_backend_bucket.website-backend.self_link
+  }
+}
+
+# GCP HTTP ELB
+resource "google_compute_target_http_proxy" "website-proxy" {
+  name = "website-target-proxy"
+  url_map = google_compute_url_map.website-map.self_link
+}
+
+# GCP Forwarding rule
+resource "google_compute_global_forwarding_rule" "default" {
+  name = "website-forwarding-rule"
+  load_balancing_scheme = "EXTERNAL"
+  ip_address = google_compute_global_address.website_ip.address
+  ip_protocol = "TCP"
+  port_range = "80"
+  target = google_compute_target_http_proxy.website-proxy.self_link
+}
